ae5941e16be7221bde99656bc812bcc510afa1e0
DiScOrDaNt <ethanhulse.work@gmail.com>
2023-04-19 16:51:34 -0600

    added assignment 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,60 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("Enter your grade percentage:");
+        int gradePercentage = Convert.ToInt32(Console.ReadLine());
+
+        string letter;
+        if (gradePercentage >= 90) {
+            letter = "A";
+        } else if (gradePercentage >= 80) {
+            letter = "B";
+        } else if (gradePercentage >= 70) {
+            letter = "C";
+        } else if (gradePercentage >= 60) {
+            letter = "D";
+        } else {
+            letter = "F";
+        }
+
+        Console.WriteLine("Your letter grade is: " + letter);
+
+        if (gradePercentage >= 70) {
+            Console.WriteLine("Congratulations! You passed the course.");
+        } else {
+            Console.WriteLine("Better luck next time. Keep trying!");
+        }
+
+        // Stretch Challenge
+        string sign;
+        int lastDigit = gradePercentage % 10;
+        if (letter == "A" && lastDigit >= 7) {
+            sign = "+";
+        } else if (letter == "B" && lastDigit >= 7) {
+            sign = "+";
+        } else if (letter == "C" && lastDigit >= 7) {
+            sign = "+";
+        } else if (letter == "D" && lastDigit >= 7) {
+            sign = "+";
+        } else if (letter == "A" && lastDigit < 3) {
+            sign = "-";
+        } else if (letter == "B" && lastDigit < 3) {
+            sign = "-";
+        } else if (letter == "C" && lastDigit < 3) {
+            sign = "-";
+        } else if (letter == "D" && lastDigit < 3) {
+            sign = "-";
+        } else {
+            sign = "";
+        }
+
+        // Handle exceptional cases
+        if (letter == "A" && lastDigit >= 3 && lastDigit <= 6) {
+            letter += "-";
+        } else if (letter == "F" && (lastDigit >= 3 || lastDigit <= 6)) {
+            letter = "F";
+        }
+
+        Console.WriteLine("Your final grade is: " + letter + sign);
     }
 }
