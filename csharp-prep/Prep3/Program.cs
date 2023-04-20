bac9b443e181c58b60053d14b73c84300562cd43
DiScOrDaNt <ethanhulse.work@gmail.com>
2023-04-19 19:17:01 -0600

    Completed Prep3-Prep5 HW

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,84 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random random = new Random();
+        int magicNumber = random.Next(1, 101);
+        int guess;
+        int guessCount = 0;
+        string guessDirection;
+
+        Console.WriteLine("Guess My Number Game");
+        Console.WriteLine("I'm thinking of a number between 1 and 100.");
+        Console.WriteLine();
+
+        do
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+            guessCount++;
+
+            if (guess < magicNumber)
+            {
+                guessDirection = "Higher";
+            }
+            else if (guess > magicNumber)
+            {
+                guessDirection = "Lower";
+            }
+            else
+            {
+                guessDirection = "You guessed it!";
+            }
+
+            Console.WriteLine(guessDirection);
+            Console.WriteLine();
+
+        } while (guess != magicNumber);
+
+        Console.WriteLine($"It took you {guessCount} guesses to get the number.");
+        Console.WriteLine();
+        Console.Write("Do you want to play again? (yes or no) ");
+        string playAgain = Console.ReadLine();
+
+        while (playAgain == "yes")
+        {
+            magicNumber = random.Next(1, 101);
+            guessCount = 0;
+
+            Console.WriteLine("Guess My Number Game");
+            Console.WriteLine("I'm thinking of a number between 1 and 100.");
+            Console.WriteLine();
+
+            do
+            {
+                Console.Write("What is your guess? ");
+                guess = int.Parse(Console.ReadLine());
+                guessCount++;
+
+                if (guess < magicNumber)
+                {
+                    guessDirection = "Higher";
+                }
+                else if (guess > magicNumber)
+                {
+                    guessDirection = "Lower";
+                }
+                else
+                {
+                    guessDirection = "You guessed it!";
+                }
+
+                Console.WriteLine(guessDirection);
+                Console.WriteLine();
+
+            } while (guess != magicNumber);
+
+            Console.WriteLine($"It took you {guessCount} guesses to get the number.");
+            Console.WriteLine();
+            Console.Write("Do you want to play again? (yes or no) ");
+            playAgain = Console.ReadLine();
+        }
+
+        Console.WriteLine("Thanks for playing!");
     }
 }
--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,45 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        int number;
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        do
+        {
+            Console.Write("Enter number: ");
+            number = int.Parse(Console.ReadLine());
+            if (number != 0)
+            {
+                numbers.Add(number);
+            }
+        } while (number != 0);
+
+        int sum = numbers.Sum();
+        double average = numbers.Average();
+        int max = numbers.Max();
+
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The largest number is: {max}");
+
+        List<int> positiveNumbers = numbers.Where(n => n > 0).ToList();
+        int smallestPositive = positiveNumbers.OrderBy(n => n).First();
+
+        Console.WriteLine($"The smallest positive number is: {smallestPositive}");
+
+        List<int> sortedNumbers = numbers.OrderBy(n => n).ToList();
+
+        Console.WriteLine("The sorted list is:");
+        foreach (int n in sortedNumbers)
+        {
+            Console.WriteLine(n);
+        }
     }
 }
