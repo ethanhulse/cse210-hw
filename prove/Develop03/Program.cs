bb67193436128900aef1ee76d7e8b35e8c1dd05b
DiScOrDaNt <ethanhulse.work@gmail.com>
2023-05-18 14:37:16 -0600

    Team activity

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -1,9 +1,74 @@
 using System;
+using System.Collections.Generic;
 
-class Program
+
+public class Program
 {
-    static void Main(string[] args)
+    public static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop03 World!");
+
+        Scripture myScripture = new Scripture("D&C 6:36", "Look unto me in every thought; doubt not, fear not.");
+
+        myScripture.DisplayScripture();
+
+
+        while(true)
+        {
+            Console.WriteLine("Press 'Enter' to continue or type 'quit' to exit.");
+
+            string input = Console.ReadLine();
+            if(input == "quit")
+            {
+                break;
+            }
+
+            myScripture.HideRandomWord();
+            myScripture.DisplayScripture();
+
+            if (myScripture.GetUnhiddenWords().Count == 0)
+            {
+                break;
+            }
+        }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+3 Class objects
+
+Scripture Parent
+    Reference Child/Parent
+        Word Child
+
+
+
+Constructors Scripture
+
+Scripture(Reference)
+Scripture(Reference, Reference_Text)
+
+//Setters/Getters
+
+string GetReference();
+string GetReference_Text();
+new scripture(John 1 1-4, "verse1", "verse2"...) 
+
+
+
+
+*/
